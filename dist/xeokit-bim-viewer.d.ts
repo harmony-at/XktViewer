0794e6f7286f82047ef86865a2a77922fa96710a
hieuqd <hieuqd@harmony-at.vn>
2024-05-27 13:49:26 +0700

    code: declare type

--- dist/xeokit-bim-viewer.d.ts
+++ dist/xeokit-bim-viewer.d.ts
@@ -1,1 +1,4 @@
 declare module "xkt-viewer";
+declare module "@harmony-at/xkt-viewer/dist/xeokit-bim-viewer.es";
+declare module "@harmony-at/xkt-viewer/dist/messages";
+declare module "@harmony-at/xkt-viewer/dist/locales/messages";
